eb829e89ca67b2c3ec28cea91f889c0d5ed85489
Dylan Bremner <61300825+bremnerdc@users.noreply.github.com>
2020-07-05 20:49:17 -0700

    deleted some console logs and commented out code

--- config/orm.js
+++ config/orm.js
@@ -1,11 +1,5 @@
-// Import MySQL connection.
 const connection = require("../config/connection");
 
-// Helper function for SQL syntax.
-// Let's say we want to pass 3 values into the mySQL query.
-// In order to write the query, we need 3 question marks.
-// The above helper function loops through and creates an array of question marks - ["?", "?", "?"] - and turns it into a string.
-// ["?", "?", "?"].toString() => "?,?,?";
 function printQuestionMarks(num) {
     var arr = [];
   
@@ -16,30 +10,21 @@ function printQuestionMarks(num) {
     return arr.toString();
   }
   
-  // Helper function to convert object key/value pairs to SQL syntax
   function objToSql(ob) {
     var arr = [];
   
-    // loop through the keys and push the key/value as a string int arr
     for (var key in ob) {
       var value = ob[key];
-      // check to skip hidden properties
       if (Object.hasOwnProperty.call(ob, key)) {
-        // if string with spaces, add quotations (Lana Del Grey => 'Lana Del Grey')
         if (typeof value === "string" && value.indexOf(" ") >= 0) {
           value = "'" + value + "'";
         }
-        // e.g. {name: 'Lana Del Grey'} => ["name='Lana Del Grey'"]
-        // e.g. {sleepy: true} => ["sleepy=true"]
         arr.push(key + "=" + value);
       }
     }
-  
-    // translate array of strings to a single comma-separated string
     return arr.toString();
   }
-  
-  // Object for all our SQL statement functions.
+
   var orm = {
     selectAll: function(tableInput, cb) {
       var queryString = "SELECT * FROM " + tableInput + ";";
@@ -70,15 +55,9 @@ function printQuestionMarks(num) {
         cb(result);
       });
     },
-    // An example of objColVals would be {burger_name: BBQ Burger, devoured: true}
-    updateOne: function(table, condition, cb) {
-      // console.log(objColVals);
-      // console.log("hi");
-      // console.log(objToSql(objColVals))
-      console.log(condition + "on orm");
 
+    updateOne: function(table, condition, cb) {
       var queryString = "UPDATE " + table;
-  
       queryString += " SET ";
       queryString += "devoured = " + false;
       queryString += " WHERE ";
@@ -93,25 +72,7 @@ function printQuestionMarks(num) {
         cb(result);
       });
     }
-
-
-
-    // ADD COMMA ABOVE TO PUT BACK IN DELETE
-    // delete: function(table, condition, cb) {
-    //   var queryString = "DELETE FROM " + table;
-    //   queryString += " WHERE ";
-    //   queryString += condition;
-  
-    //   connection.query(queryString, function(err, result) {
-    //     if (err) {   
-    //       throw err;
-    //     }
-  
-    //     cb(result);
-    //   });
-    // }
   };
   
-  // Export the orm object for the model (cat.js).
   module.exports = orm;
   
